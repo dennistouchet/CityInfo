d2b2e7fa6ddfad4ef8100cea3ebe3a0fa4d05f0b
Dennis Touchet <admin@dennistouchet.com>
2017-11-30 23:55:29 -0600

    added configuration. setup configuration to be used instead of hardcoded values for email address. created builder manually (necessary for Core 1 - to be updated to steamlined v2 in next iteration.)

--- CityInfo.API/Services/CloudMailService.cs
+++ CityInfo.API/Services/CloudMailService.cs
@@ -9,8 +9,8 @@ namespace CityInfo.API.Services
 {
     public class CloudMailService : IMailService
     {
-        private string _mailTo = "admin@dennistouchet.com";
-        private string _mailFrom = "noreply@dennistouchet.com";
+        private string _mailTo = Startup.Configuration["mailSettings:mailToAddress"];
+        private string _mailFrom = Startup.Configuration["mailSettings:mailFromAddress"];
 
         public void Send(string subject, string body)
         {
--- CityInfo.API/Startup.cs
+++ CityInfo.API/Startup.cs
@@ -12,11 +12,24 @@ using Microsoft.Extensions.Logging;
 using NLog.Extensions.Logging;
 using CityInfo.API.Services;
 using CityInfo.API.Interfaces;
+using Microsoft.Extensions.Configuration;
 
 namespace CityInfo.API
 {
     public class Startup
     {
+        // config init
+        public static IConfigurationRoot Configuration;
+
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional:false, reloadOnChange:true);
+
+            Configuration = builder.Build();
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
